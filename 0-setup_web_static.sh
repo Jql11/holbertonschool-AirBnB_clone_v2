04d8967b8afcebcfcb08d29f59fe1fce1076bb29
Jql11 <jaqueling@126.com>
2022-08-25 00:37:05 +0000

    add restart

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,10 +3,11 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
+sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 sudo sh -c 'echo "Holberton School" > /data/web_static/releases/test/index.html'
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx restart
