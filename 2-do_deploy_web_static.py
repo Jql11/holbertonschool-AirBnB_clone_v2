11af0cc7a1a21514cbe5893ae3605216ee050b5c
Jql11 <jaqueling@126.com>
2022-08-25 10:31:16 +0000

    change file existence method

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-"""distributes an archive to server using function do_deploy
+"""
+Fabric script creates and distributes
+an archive to web servers
 """
 from fabric.api import env, put, run
-from os.path import exists
+import os.path
 env.hosts = ['54.90.130.172', '52.90.163.197']
 
 
 def do_deploy(archive_path):
-    """distributing archive to server"""
-    if exists(archive_path) is False:
+    """
+    Deploy archive to web server
+    """
+    if os.path.isfile(archive_path) is False:
         return False
     try:
         get_name = archive_path.split("/")[-1]
         no_ext = get_name.split(".")[0]
-        folder_path = "/data/web_static/releases/{}/".format(no_ext)
-        put("archive_path", "/tmp")
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp{} -C {}/".format(get_name, folder_path))
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(get_name, path_no_ext))
         run("rm /tmp/{}".format(get_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
         return True
     except Exception:
         return False
