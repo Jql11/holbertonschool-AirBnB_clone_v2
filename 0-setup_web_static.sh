ce29a01f0d54b75bf38611896fc5090ea23cd2b7
Jql11 <jaqueling@126.com>
2022-08-25 03:45:35 +0000

    change append location so checker will be happy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,5 +9,5 @@ sudo mkdir -p /data/web_static/shared
 sudo sh -c 'echo "Holberton School" > /data/web_static/releases/test/index.html'
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '/listen \[::\]:80 default_server;/a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}' /etc/nginx/sites-available/default
+sudo sed -i '29i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
