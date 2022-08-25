0f87cfe1aaad6c83fc43db0d451d94e1b2fcb351
Jql11 <jaqueling@126.com>
2022-08-25 01:20:44 +0000

    add documentation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,11 +3,11 @@
 folder of your AirBnB Clone repo, using the function do_pack
 """
 from fabric.api import local
-import tarfile
 import time
 
 
 def do_pack():
+    """all files in th folder are added to the archive"""
     current_time = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
