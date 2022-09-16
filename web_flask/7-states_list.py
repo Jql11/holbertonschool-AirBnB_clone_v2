2163fe2a46a1bdefd867f144083f46fff9035f1b
Jql11 <jaqueling@126.com>
2022-09-16 11:39:20 +0000

    execute

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,7 +18,7 @@ def teardown_db(exception):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    return render_template("7-states_list.html", dict=storage.all("State"))
+    return render_template("7-states_list.html", dict=storage.all(State))
 
 
 if __name__ == "__main__":
