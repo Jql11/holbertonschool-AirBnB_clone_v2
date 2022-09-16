14e9fbdaba0a3c7f19ae45745f36a873abd73870
Jql11 <jaqueling@126.com>
2022-09-16 09:49:01 +0000

    add file

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application
+"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models import State
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def shutdown_session(exception):
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    return render_template("7-states_list.html", dict=storage.all(State))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
