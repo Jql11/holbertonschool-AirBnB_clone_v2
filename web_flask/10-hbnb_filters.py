c11214b47ee8d669f1ad8345f8a9949def35794c
Jql11 <jaqueling@126.com>
2022-09-19 06:03:31 +0000

    import amenity

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -7,20 +7,23 @@ from flask import Flask
 from flask import render_template
 from models import storage
 from models import State
+from models import Amenity
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_db(exception=None):
+    """remove the current SQLAlchemy Session:"""
     storage.close()
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
+    """display a HTML page"""
     return render_template("10-hbnb_filters.html",
                            dict=storage.all(State),
                            amenity_dict=storage.all(Amenity))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
